073fbff73a0a9b7591ee88605117458acf444c5d
naparyspotkanie <napary_spotkanie.0q@icloud.com>
2024-05-08 18:07:06 +0200

    Initial Commit

---
+++ Rock Paper Scissor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rock Paper Scissor
+//
+//  Created by Mateusz Król on 08/05/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
